16a3ab031860ebd1e28335d09be9e2e8752d7f26
ZhMi <zmmingtiandege1314@126.com>
2016-03-08 22:31:11 +0800

    filter data which contains key words

--- health_care_finance_spider/constant_config.py
+++ health_care_finance_spider/constant_config.py
@@ -3,4 +3,4 @@ __author__ = 'Zhmi'
 # -*- coding: utf8 -*-
 
 kpmg_data_path = "G:\Health-Care-Financing-Data-Crawling\health_care_finance_spider\data.csv"
-key_words = ['medical','finance','care','financing','raises','million']
+key_words = ['medical','finance','financial','care','financing','raises','health','medical','raising','capital','refinance']
--- health_care_finance_spider/extract_health_care_financing_article.py
+++ health_care_finance_spider/extract_health_care_financing_article.py
@@ -22,22 +22,42 @@ class extract_health_care_article(object):
             print self.article_list # ['url', 'main_title', 'second_title', 'publish_time']
 
     def data_pretreat(self):
-        data_list = map(lambda x:[x[0],list(set(x[1]+x[2])),x[3][5:]+ x[3][1:2] +x[3][2:3]],self.article_list[1:]) # ['Thu', 'Jan', '01', '05:00:00', 'UTC', '2015']
-        return data_list # ['2015', 'Aug', '01']
+        '''
+        function description:
+            date form of raw data is ['Thu', 'Jan', '01', '05:00:00', 'UTC', '2015']
+            filter correct form of time as follows: ['2015', 'Aug', '01']
+            combine main title and second title
+        '''
+        data_list = map(lambda x:[x[0],list(set(x[1]+x[2])),x[3][5:]+ x[3][1:2] +x[3][2:3]],self.article_list[1:])
+        return data_list
 
-    def data_filter(record,key_words):
-        # ret = list(set(a) ^ set(b))
-        intersection = list(set(record[1:2]) ^ list(set(key_words)))
-        return len(intersection)
+    def filter_contains_topic_data(self,data_list):
+        '''
+        data form:
+            every piece of data is a list contains words of main title and second title
+            key_words is a list contains key words
+        function description:
+            if record contain contains key words,intersection of those two list is not null
+            change words vector into length of intersection
+            return the data which contains key words(length of intersection is not 0)
+        '''
+        key_words = constant_config.key_words
+        for index in xrange(len(data_list)): # data_list: [['url', ['a', 'welcome', ], ['2015', 'nov', '23']]
+            words_vector = data_list[index][1]
+            intersection = list(set(words_vector) & set(key_words))
+            data_list[index][1] = len(intersection)
+            if 1 == len(intersection):
+                print "***  words_vector  ***",len(words_vector)
+                print "***  content of words_vector ***",words_vector
+                print "***  intersection  ***",len(intersection)
+                print "\n"
+        contain_topic_data = filter(lambda x: x[1] != 0,data_list)
+        return contain_topic_data
 
 if __name__ == '__main__':
     object = extract_health_care_article()
     object.__init__()
     article_list = object.read_file()
-    date_list = object.data_pretreat()
-    key_words = constant_config.key_words
-    conbine_list = map(lambda x: object.data_filter,key_words,date_list)
-
-
-
-
+    filtered_data_list = object.data_pretreat()
+    contain_topic_data = object.filter_contains_topic_data(filtered_data_list)
+    print "contain_topic_data:%s"%contain_topic_data
