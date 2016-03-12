1cef197eb829c852d4c75237fddbd0b7ddfbe6b7
ZhMi <zmmingtiandege1314@126.com>
2016-03-12 15:52:59 +0800

    filter date belong to a certain date[year,month] and contains key words

--- health_care_finance_spider/constant_config.py
+++ health_care_finance_spider/constant_config.py
@@ -4,3 +4,5 @@ __author__ = 'Zhmi'
 
 kpmg_data_path = "G:\Health-Care-Financing-Data-Crawling\health_care_finance_spider\data.csv"
 key_words = ['medical','finance','financial','care','financing','raises','health','medical','raising','capital','refinance']
+year = ['2015']
+month = ['jun']
--- health_care_finance_spider/extract_health_care_financing_article.py
+++ health_care_finance_spider/extract_health_care_financing_article.py
@@ -55,21 +55,17 @@ class extract_health_care_article(object):
         contain_topic_data = filter(lambda x: x[1] != 0,data_list)
         return contain_topic_data
 
-    def filter_record_belongs_particular_time(self,data_list):
-        for index in xrange(len(data_list)):
+    def filter_record_belongs_particular_time(self,data_list,year,month):
+        date_filtered_data = filter(lambda x: x[2][0:1] == year and x[2][1:2] == month,data_list)
+        date_filtered_url_date = map(lambda x:[x[0],x[2]],date_filtered_data)
+        return date_filtered_url_date
 
-            data_list[index][2][1] = self.date_dict[data_list[index][2][1]]
-            '''
-            d1 = datetime.datetime(2005, 2, 16)
-            d2 = datetime.datetime(2004, 12, 31)
-            print (d1 - d2).days
-            '''
-            year_num = int(data_list[index][2][0])
-            data_list[index][2][0] = year_num
-            day_num = int(data_list[index][2][2])
-            data_list[index][2][2] = day_num
-
-        return data_list
+    def write_csv_file(self,data_list,category,file_name):
+        csvfile = file('filtered_date_key_words_date','wb')
+        writer = csv.writer(csvfile)
+        writer.writerow(['url','date'])
+        writer.writerows(data_list)
+        csvfile.close()
 
 if __name__ == '__main__':
     object = extract_health_care_article()
@@ -78,4 +74,9 @@ if __name__ == '__main__':
     filtered_data_list = object.data_pretreat()
     contain_topic_data = object.filter_contains_topic_data(filtered_data_list)
     print "contain_topic_data:%s"%contain_topic_data
-    object.filter_record_belongs_particular_time(contain_topic_data)
+    date_filtered_url_date = object.filter_record_belongs_particular_time(contain_topic_data,constant_config.year,constant_config.month)
+    print "date_filted_url_date:%s"%date_filtered_url_date
+    category = ['url','date']
+    file_name = 'filtered_date_key_words_date'
+    object.write_csv_file(date_filtered_url_date,category,file_name)
+
