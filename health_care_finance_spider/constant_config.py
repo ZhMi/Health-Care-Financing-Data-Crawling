394b0def8cba2057ba8a321010a6e3ad3fa453d8
ZhMi <zmmingtiandege1314@126.com>
2016-03-12 21:22:54 +0800

    filter date belong to a certain date[year,month] and contains key words

--- health_care_finance_spider/constant_config.py
+++ health_care_finance_spider/constant_config.py
@@ -6,3 +6,5 @@ kpmg_data_path = "G:\Health-Care-Financing-Data-Crawling\health_care_finance_spi
 key_words = ['medical','finance','financial','care','financing','raises','health','medical','raising','capital','refinance']
 year = ['2015']
 month = ['jun']
+
+root_urls = [ "https://home.kpmg.com/xx/en/home.html","http://www.pwc.com/gx/en/industries/healthcare/publications.html"]
--- health_care_finance_spider/spider_main.py
+++ health_care_finance_spider/spider_main.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals # 解决控制台输出中文乱码问题
 import url_manager , html_downloader , html_parser , html_outputer
 import datetime
+import constant_config
 
 class SpiderMain(object):
     def __init__(self):
@@ -9,7 +10,7 @@ class SpiderMain(object):
         self.downloader = html_downloader.HtmlDownloader()
         self.parser = html_parser.HtmlParser()
         self.outputer = html_outputer.HtmlOutputer()
-    def craw(self,root_url):
+    def craw(self,root_url,num):
         count = 1
         self.urls.add_new_url(root_url)
         while self.urls.has_new_url():
@@ -20,7 +21,7 @@ class SpiderMain(object):
                 new_urls,new_data = self.parser.parse(new_url,html_cont)
                 self.urls.add_new_urls(new_urls)
                 self.outputer.collect_data(new_data)
-                if count == 800:
+                if count == num :
                     break
                 count = count + 1
             except:
@@ -31,11 +32,13 @@ class SpiderMain(object):
 
 if __name__ == "__main__":
     start_time = datetime.datetime.now()
-    # root_url = "http://baike.baidu.com/view/21"
     # https://home.kpmg.com/xx/en/home.html
     root_url =  "https://home.kpmg.com/xx/en/home.html"
     obj_spider = SpiderMain() # 创建爬虫总调度程序
-    obj_spider.craw(root_url) # start spider
+    num = 10
+    for url_index in xrange(1):
+        root_url = constant_config.root_urls[url_index]
+        obj_spider.craw(root_url,num) # start spider
     end_time = datetime.datetime.now()
     run_time = (end_time - start_time).seconds
     print "***  run_time:***    %s s    "%run_time
